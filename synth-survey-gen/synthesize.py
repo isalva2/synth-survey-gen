48dfafdc1274e3d2e8df9dd224ff43403591e7d2
Isaac Salvador <isalva2@uic.edu>
2025-01-28 14:50:53 +0100

    rewriting with Ollama

---
+++ synth-survey-gen/synthesize.py
@@ -0,0 +1,19 @@
+import ollama
+import json
+from typing import List, Dict
+from pathlib import Path
+
+
+def generate_population_sample(n: int) -> List[List[str]]:
+    """
+    Returns a list of string attributes for `n` many agents. Uses iterative proportional fitting.
+    """
+    pass
+
+
+def prepare_survey_questions(population_sample):
+    pass
+
+
+def query_sample(population_sample: List[List[str]], user_messages: List[Dict[str]]):
+    pass
